fb40e63801a81999ec71f4af057e836fc09b8277
Isaac Kwan <g@isaac.pw>
2018-05-13 02:28:56 +0800

    reverted, back to asyncstorage

--- src/default-options-reactnative.js
+++ src/default-options-reactnative.js
@@ -1,24 +1,29 @@
 'use strict'
 
-const memoryDs = require("interface-datastore/src/memory");
-
 // Default configuration for a repo in React Native
 module.exports = {
   lock: 'memory',
   storageBackends: {
-    root: memoryDs,
-    blocks: memoryDs,
-    keys: memoryDs,
-    datastore: memoryDs
+    root: require('datastore-level'),
+    blocks: require('datastore-level'),
+    keys: require('datastore-level'),
+    datastore: require('datastore-level')
   },
   storageBackendOptions: {
     root: {
+      db: require('asyncstorage-down'),
+      extension: ''
     },
     blocks: {
+      sharding: false,
+      db: require('asyncstorage-down')
     },
     keys: {
+      sharding: false,
+      db: require('asyncstorage-down')
     },
     datastore: {
+      db: require('asyncstorage-down')
     }
   }
 }
